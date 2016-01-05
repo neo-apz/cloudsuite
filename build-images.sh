b577338c454357f8d26dcdc4d56cc6571af2ff25
neo <arash.p.z@gmail.com>
2016-01-05 13:58:08 +0100

    Extra tagging is removed.

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,6 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
 export REPO=arashpz/data-caching-ci
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 docker build -t server ./benchmarks/data-caching/server
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+# docker tag $REPO:$COMMIT $REPO:$TAG
+# docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
